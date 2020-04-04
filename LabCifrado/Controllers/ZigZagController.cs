47d69cc1c9fca8aed1ff9cec2a2389dfa53f07f9
genecc15 <genecc15@gmail.com>
2020-04-04 16:58:39 -0600

    Cifrado

---
+++ LabCifrado/Controllers/ZigZagController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabCifrado.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ZigZagController : ControllerBase
+    {
+        public static IWebHostEnvironment _environment;
+        private readonly ZigZagMetodos CesarCifrado = new ZigZagMetodos();
+        private readonly ZigZagMetodos CesarDesc = new ZigZagMetodos();
+
+        public ZigZagController (IWebHostEnvironment env)
+        {
+            _environment = env;
+        }
+
+        public class FileUploadApi
+        {
+            public IFormFile Files { get; set; }
+            public string Niveles { get; set; }
+            public string Nombre { get; set; }
+        }
+
+        [Route("/Cipher/ZigZag")]
+        [HttpPost]
+        public async Task<string> UploadFileText([FromForm] FileUploadApi objFile, [FromForm] FileUploadApi key, [FromForm]FileUploadApi nombreobj)
+        {
+            try
+            {
+                if (objFile.Files.Length > 0)
+                {
+                    int i;
+   
+                    string nombre = nombreobj.Nombre;
+                    string clave = key.Niveles;
+                    bool resultado = false;
+                    if (int.TryParse(clave, out i))
+                    {
+                        if (i < 10000 && i > 0)
+                        {
+                            if (!Directory.Exists(_environment.WebRootPath + "\\UploadZigZag\\")) Directory.CreateDirectory(_environment.WebRootPath + "\\UploadZigZag\\");
+                            using var _fileStream = System.IO.File.Create(_environment.WebRootPath + "\\UploadZigZag\\" + objFile.Files.FileName);
+                            objFile.Files.CopyTo(_fileStream);
+                            _fileStream.Flush();
+                            _fileStream.Close();
+                            resultado = true;
+                            int niv = Convert.ToInt32(clave);
+                            ZigZagCifrado2(objFile, niv, nombre);
+                        }
+                        else
+                        {
+                            return "La contraseña está fuera del rango";
+                        }
+                    }
+                    else
+                    {
+                        return "La contraseña debe consistir de números";
+                    }
+                    return "Archivo subido y cifrado correctamente";
+                }
+                else return "Archivo Vacio";
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message.ToString();
+            }
+        }
+
+        public void ZigZagCifrado2(FileUploadApi objFile, int contra, string nombre)
+        {
+            string archivo = nombre;
+            int clave = contra;
+            string[] FileName1 = objFile.Files.FileName.Split(".");
+            ZigZagMetodos.ZigZagAlgortimo(_environment.WebRootPath + "\\UploadZigZag\\" + objFile.Files.FileName, _environment.WebRootPath + "\\UploadZigZag\\" + archivo + ".txt", clave);
+        }
+    }
+}
---
+++ LabCifrado/ZigZagMetodos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.IO;
+
+namespace LabCifrado
+{
+    public class ZigZagMetodos
+    {
+        public static string CurrentFile = "";
+        public static void ZigZagAlgortimo(string Rpath, string Wpath, int llave)
+        {
+            Cifrado(Rpath, Wpath, llave);
+        }
+        public static void Cifrado(string Rpath, string Wpath, int Corrimiento)
+        {
+            string Data = System.IO.File.ReadAllText(Rpath, Encoding.Default);
+            string mensaje = Data;
+            var lineas = new List<StringBuilder>();
+            int niveles = Corrimiento;
+            for (int i = 0; i < niveles; i++)
+            {
+                lineas.Add(new StringBuilder());
+            }
+            int ActualL = 0;
+            int Direccion = 1;
+            //For para saber donde empezamos
+
+            for (int i = 0; i < mensaje.Length; i++)
+            {
+                lineas[ActualL].Append(mensaje[i]);
+
+                if (ActualL == 0)
+                    Direccion = 1;
+                else if (ActualL == niveles - 1)
+                    Direccion = -1;
+
+                ActualL += Direccion;
+            }
+            StringBuilder CifradoFinal = new StringBuilder();
+
+            //Saber donde se encuentra cada caracter
+            for (int i = 0; i < niveles; i++)
+                CifradoFinal.Append(lineas[i].ToString());
+
+            string Cifrados = CifradoFinal.ToString();
+
+            File.WriteAllText(Wpath, Cifrados);
+            CurrentFile = Wpath;
+        }
+    }
+}
