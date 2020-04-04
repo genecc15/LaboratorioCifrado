cb52b5d120a8a5145c856ad328780c1c51b389cb
genecc15 <genecc15@gmail.com>
2020-04-04 16:30:56 -0600

    CesarDescifrado

--- LabCifrado/CesarMetodos.cs
+++ LabCifrado/CesarMetodos.cs
@@ -13,16 +13,16 @@ namespace LabCifrado
         public static string CurrentFile = "";
         public static void CesarAlgoritmo(string Rpath, string Wpath, string llave)
         {
-            Comprimir(Rpath, Wpath, llave);
+            Cifrado(Rpath, Wpath, llave);
         }
-        public static void CesarAlgoritmo2(string Rpath, string Wpath)
+        public static void CesarAlgoritmo2(string Rpath, string Wpath, string llave)
         {
-
+            Descifrar(Rpath, Wpath, llave);
         }
 
-        #region Comprimir
+        #region Cifrar
 
-        public static void Comprimir(string Rpath, string Wpath, string llave)
+        public static void Cifrado(string Rpath, string Wpath, string llave)
         {
             #region Alfabeto Mayusculas
             List<char> AlfabetoM = new List<char>();
@@ -98,7 +98,7 @@ namespace LabCifrado
             List<char> Diferentes = AlfabetoM.Except(ListaClave).ToList();
             List<char> Repetidos = (ListaClave.AsQueryable().Intersect(AlfabetoM)).ToList(); //Expresion Lamba para encontrar repetidos
             ListaFinal = Repetidos.Union(Diferentes).ToList();
-            var DiccionarioM = AlfabetoM.Zip(ListaFinal, (k, v) => new { k, v }).ToDictionary(x => x.k, x => x.v); //Combinar listas y volverlas diccionario
+            var DiccionarioM = ListaFinal.Zip(AlfabetoM, (k, v) => new { k, v }).ToDictionary(x => x.k, x => x.v); //Combinar listas y volverlas diccionario
             #endregion
             #region Diccionario Minusculas
             List<char> DiferentesP = AlfabetoP.Except(ListaClave2).ToList();
@@ -134,6 +134,126 @@ namespace LabCifrado
             File.WriteAllText(Wpath, CifradoCompleto);
             CurrentFile = Wpath;
         }
+        #endregion
+
+        #region Descifrar
+
+       public static void Descifrar(string Rpath, string Wpath, string llave)
+        {
+
+            #region Alfabeto Mayusculas
+            List<char> AlfabetoM = new List<char>();
+            AlfabetoM.Add('A');
+            AlfabetoM.Add('B');
+            AlfabetoM.Add('C');
+            AlfabetoM.Add('D');
+            AlfabetoM.Add('E');
+            AlfabetoM.Add('F');
+            AlfabetoM.Add('G');
+            AlfabetoM.Add('H');
+            AlfabetoM.Add('I');
+            AlfabetoM.Add('J');
+            AlfabetoM.Add('K');
+            AlfabetoM.Add('L');
+            AlfabetoM.Add('M');
+            AlfabetoM.Add('N');
+            AlfabetoM.Add('O');
+            AlfabetoM.Add('P');
+            AlfabetoM.Add('Q');
+            AlfabetoM.Add('R');
+            AlfabetoM.Add('S');
+            AlfabetoM.Add('T');
+            AlfabetoM.Add('U');
+            AlfabetoM.Add('V');
+            AlfabetoM.Add('W');
+            AlfabetoM.Add('X');
+            AlfabetoM.Add('Y');
+            AlfabetoM.Add('Z');
+            AlfabetoM.Add(' ');
+            #endregion
+            #region Alfabeto Minusculas
+            List<char> AlfabetoP = new List<char>();
+            AlfabetoP.Add('a');
+            AlfabetoP.Add('b');
+            AlfabetoP.Add('c');
+            AlfabetoP.Add('d');
+            AlfabetoP.Add('e');
+            AlfabetoP.Add('f');
+            AlfabetoP.Add('g');
+            AlfabetoP.Add('h');
+            AlfabetoP.Add('i');
+            AlfabetoP.Add('j');
+            AlfabetoP.Add('k');
+            AlfabetoP.Add('l');
+            AlfabetoP.Add('m');
+            AlfabetoP.Add('n');
+            AlfabetoP.Add('o');
+            AlfabetoP.Add('p');
+            AlfabetoP.Add('q');
+            AlfabetoP.Add('r');
+            AlfabetoP.Add('s');
+            AlfabetoP.Add('t');
+            AlfabetoP.Add('u');
+            AlfabetoP.Add('v');
+            AlfabetoP.Add('w');
+            AlfabetoP.Add('x');
+            AlfabetoP.Add('y');
+            AlfabetoP.Add('z');
+            AlfabetoP.Add(' ');
+            #endregion
+
+            string data = System.IO.File.ReadAllText(Rpath, Encoding.Default);
+            #region Listas
+            string clave = llave.ToUpper();
+            string claveM = llave.ToLower();
+            List<char> ListaFinal = new List<char>(); //Lista que sera el abecedario modificado
+            List<char> ListaFinal2 = new List<char>();
+            List<char> ListaClave = clave.ToList();
+            List<char> ListaClave2 = claveM.ToList();
+            #endregion
+            #region Diccionario Mayusculas
+            List<char> Diferentes = AlfabetoM.Except(ListaClave).ToList();
+            List<char> Repetidos = (ListaClave.AsQueryable().Intersect(AlfabetoM)).ToList(); //Expresion Lamba para encontrar repetidos
+            ListaFinal = Repetidos.Union(Diferentes).ToList();
+            var DiccionarioM = ListaFinal.Zip(AlfabetoM, (k, v) => new { k, v }).ToDictionary(x => x.k, x => x.v); //Combinar listas y volverlas diccionario
+            #endregion
+            #region Diccionario Minusculas
+            List<char> DiferentesP = AlfabetoP.Except(ListaClave2).ToList();
+            List<char> RepetidosP = (ListaClave2.AsQueryable().Intersect(AlfabetoP)).ToList(); //Expresion Lamba para encontrar repetidos
+            ListaFinal2 = RepetidosP.Union(DiferentesP).ToList();
+            var DiccionarioP = ListaFinal2.Zip(AlfabetoP, (k, v) => new { k, v }).ToDictionary(x => x.k, x => x.v); //Combinar listas y volverlas diccionario
+            #endregion
+            List<char> Descifrado = data.ToList();
+            List<char> DescifradoFinal = new List<char>();
+
+            foreach (var item in Descifrado)
+            {
+                if (DiccionarioM.ContainsKey(item))
+                {
+                    // CifradoFinal.Add(diccionario[item]);
+                    DescifradoFinal.Add(DiccionarioM[item]);
+                }
+                else if (DiccionarioP.ContainsKey(item))
+                {
+                    DescifradoFinal.Add(DiccionarioP[item]);
+                }
+                else
+                {
+                    DescifradoFinal.Add(item);
+                }
+            }
+            string DescifradoCompleto = "";
+
+            foreach (var item in DescifradoFinal)
+            {
+                DescifradoCompleto += item;
+            }
+
+            File.WriteAllText(Wpath, DescifradoCompleto);
+            CurrentFile = Wpath;
+
+        }
+
         #endregion
     }
 }
--- LabCifrado/Controllers/CesarController.cs
+++ LabCifrado/Controllers/CesarController.cs
@@ -14,7 +14,7 @@ namespace LabCifrado.Controllers
     public class CesarController : ControllerBase
     {
         public static IWebHostEnvironment _environment;
-        private readonly CesarMetodos CesarCompression = new CesarMetodos();
+        private readonly CesarMetodos CesarCifrado = new CesarMetodos();
         private readonly CesarMetodos CesarDesc = new CesarMetodos();
         public CesarController(IWebHostEnvironment env)
         {
@@ -25,16 +25,18 @@ namespace LabCifrado.Controllers
         {
             public IFormFile Files { get; set; }
             public string Clave { get; set; }
+            public string Nombre { get; set; }
         }
 
         [Route("/Cipher/Cesar")]
         [HttpPost]
-        public async Task<string> UploadFileText([FromForm] FileUploadApi objFile,[FromForm] FileUploadApi key)
+        public async Task<string> UploadFileText([FromForm] FileUploadApi objFile,[FromForm] FileUploadApi key, [FromForm]FileUploadApi nombreobj)
         {
             try
             {
                 if (objFile.Files.Length > 0)
                 {
+                    string nombre = nombreobj.Nombre;
                     string clave = key.Clave;
                     bool resultado = false;
                     if (clave.All(char.IsLetter))
@@ -47,7 +49,7 @@ namespace LabCifrado.Controllers
                             _fileStream.Flush();
                             _fileStream.Close();
                             resultado = true;
-                            CesarCompress(objFile, clave);
+                            CesarCifrado2(objFile, clave, nombre);
                         }
                         else
                         {
@@ -69,12 +71,66 @@ namespace LabCifrado.Controllers
             }
         }
 
-        public void CesarCompress(FileUploadApi objFile, string contra)
+        public void CesarCifrado2(FileUploadApi objFile, string contra, string nombre)
         {
+            string archivo = nombre;
             string clave = contra;
             string[] FileName1 = objFile.Files.FileName.Split(".");
-            CesarMetodos.CesarAlgoritmo(_environment.WebRootPath + "\\UploadCesar\\" + objFile.Files.FileName, _environment.WebRootPath + "\\UploadCesar\\" + "Cifrado.txt", clave);
+            CesarMetodos.CesarAlgoritmo(_environment.WebRootPath + "\\UploadCesar\\" + objFile.Files.FileName, _environment.WebRootPath + "\\UploadCesar\\" + archivo+".txt", clave);
         }
+
+        [Route("/Decipher/Cesar")]
+        [HttpPost]
+        public async Task<string> UploadFileCesar([FromForm] FileUploadApi objFile, [FromForm] FileUploadApi key, [FromForm]FileUploadApi nombreobj)
+        {
+            try
+            {
+                if (objFile.Files.Length > 0)
+                {
+                    string nombre = nombreobj.Nombre;
+                    string clave = key.Clave;
+                    bool resultado = false;
+                    if (clave.All(char.IsLetter))
+                    {
+                        if (clave.Distinct().Count() == clave.Length)
+                        {
+                            if (!Directory.Exists(_environment.WebRootPath + "\\UploadCesar\\")) Directory.CreateDirectory(_environment.WebRootPath + "\\UploadCesar\\");
+                            using var _fileStream = System.IO.File.Create(_environment.WebRootPath + "\\UploadCesar\\" + objFile.Files.FileName);
+                            objFile.Files.CopyTo(_fileStream);
+                            _fileStream.Flush();
+                            _fileStream.Close();
+                            resultado = true;
+                            CesarDescifrado(objFile, clave, nombre);
+                        }
+                        else
+                        {
+                            return "No se puede usar esa clave, porfavor elige una palabra con letras diferentes.";
+                        }
+                    }
+                    else
+                    {
+                        return "No se puede usar esa clave, porfavor escribe una palabra";
+                    }
+                    return "Archivo Descifrado correctamente";
+                }
+                else return "Archivo Vacio";
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message.ToString();
+            }
+
+
+        }
+        public void CesarDescifrado(FileUploadApi objFile, string contra, string nombre)
+        {
+            string archivo = nombre;
+            string clave = contra;
+            string[] FileName1 = objFile.Files.FileName.Split(".");
+            CesarMetodos.CesarAlgoritmo2(_environment.WebRootPath + "\\UploadCesar\\" + objFile.Files.FileName, _environment.WebRootPath + "\\UploadCesar\\" + archivo + ".txt", clave);
+        }
+
 
     }
 }
