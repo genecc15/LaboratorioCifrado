8248cf499beb42d661aa85ed14fbffcd382a811a
genecc15 <genecc15@gmail.com>
2020-04-04 18:38:39 -0600

    Orden

--- LabCifrado/Controllers/ZigZagController.cs
+++ LabCifrado/Controllers/ZigZagController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using LabCifrado.Cifrados;
 
 namespace LabCifrado.Controllers
 {
