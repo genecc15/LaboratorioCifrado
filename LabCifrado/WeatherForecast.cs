3bd1e0f58c573323a432ed659fe59d5fbfd46dc5
genecc15 <genecc15@gmail.com>
2020-04-04 22:16:11 -0600

    Bienvenida

--- LabCifrado/WeatherForecast.cs
+++ LabCifrado/WeatherForecast.cs
@@ -4,12 +4,8 @@ namespace LabCifrado
 {
     public class WeatherForecast
     {
-        public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string Summary { get; set; }
+        public string Bienvenida { get; set; }
+        public string Instrucciones { get; set; }
     }
 }
